1bda83828072567f45ecb152298c50df9f9938f4
Romulo Alvernaz <127262652+RomuloAlvernaz@users.noreply.github.com>
2023-12-18 22:02:34 -0300

    :sparkles:  initial commit

---
+++ alvernaz_bookstore/src/test/java/com/bookstore/alvernaz_bookstore/AlvernazBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.alvernaz_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlvernazBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
